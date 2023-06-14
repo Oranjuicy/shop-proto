066da0a0655e3c937162b6546ba4bbec4329fecd
Retief Ryke <retief.ryke@gmail.com>
2023-06-14 13:02:33 +0200

    Set up basic page

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return (
+        <header class="header">
+            <h1>SAramics Market</h1>
+        </header>
+    );
+}
+
+export default Header;

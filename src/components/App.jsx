776f3bc2fc050efe9952f0f80bd1ca5c4f2469af
Retief Ryke <retief.ryke@gmail.com>
2023-07-10 14:49:35 +0200

    Create item component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header"
 import Footer from "./Footer";
 import Namecard from "./Namecard";
+import Item from "./Item";
 
 function App() {
     return (
         <div>
             <Header />
             <Footer />
+            <Item />
             <Namecard price="R300" artistName="Retief Ryke" artworkName="Big Jug" description="A large stoneware jug" />
 
         </div>
---
+++ src/components/Item.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Item(props){
+    return (<div>
+            <img src="https://picsum.photos/seed/picsum/200/300" />
+        </div>
+    )
+}
